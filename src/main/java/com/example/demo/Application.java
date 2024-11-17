e734bce08751383553e3e17048a2adc14fa37dae
Ziyaddin <ziya.huseynov2001@mail.ru>
2024-11-17 21:33:07 +0400

    Connecting DB,create Student class and Entity,Column,Table,Sequence and create repository

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,13 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student ziya = new Student(
+                    "Ziya","Huseynov","ziya2001@mail.ru",23
+            );
+            studentRepository.save(ziya);
+        };
+    }
 }
